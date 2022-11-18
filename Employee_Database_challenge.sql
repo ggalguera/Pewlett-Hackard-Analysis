ed80204970629499bf018eac5cc39de716f5b95e
ggalguera <galguera@msn.com>
2022-11-18 16:42:22 -0500

    Update Query

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -4,7 +4,7 @@ SELECT e.emp_no,
     t.title,
     t.from_date,
 	t.to_date
-INTO retirement_titles
+-- INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -21,7 +21,27 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no ASC, to_date DESC;
 
+-- Create a table of retiring employees by title
 SELECT COUNT(title), title
+INTO retiring_titles
 FROM unique_retirement_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+-- Create a list of mentorship program elegible employees
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+d.from_date,
+d.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (d.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
